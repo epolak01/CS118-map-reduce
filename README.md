aa6d651a22f825e49bc10fe69d64e92292f46653
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-12-05 19:56:51 -0500

    Fixed mapper2, and implemented reducer2, except for popularity trend functionality

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ How to run locally:
     - Make mapper1.py and reduce1.py executable using 'chmod +x filename.py'
     - Run the following command to pipe sample_data into mapper and mapper output
     into reducer 'cat sample_data |  ./mapper1.py | ./reducer1.py'
+
+Note:
+    - Reducers expect sorted inputs
+        - For reducer to run properly give sample_data_sorted as an input to 
+        the mapper instead
--- reducer2.py
+++ reducer2.py
@@ -3,14 +3,51 @@
 from collections import defaultdict
 import sys
 view_count = defaultdict(int)
+
+pagename = None
+curr_pagename = None
+dates = []
+pageviews_per_date = []
+count = 0
+
+
 for line in sys.stdin:
     try:
         line = line.strip()
-        pagename_date, pageviews = line.split()
+        pagename, date_pageviews = line.split()
+        date, pageviews = date_pageviews.split('}')
         pageviews = int(pageviews)
     except:
         continue
-    view_count[pagename_date] += pageviews
+    # print old result, and start accumulating new result
+    if (pagename != curr_pagename):
+        if (curr_pagename != None): # print old result
+            # TODO add popularity
+            print(curr_pagename, '\t', dates, '\t', pageviews_per_date, '\t', count)
+        
+        curr_pagename = pagename
+        dates.clear()
+        pageviews_per_date.clear()
+        
+        count = pageviews
+        dates.append(date)
+        pageviews_per_date.append(pageviews)
+        
+    else:
+        dates.append(date)
+        pageviews_per_date.append(pageviews)
+        count += pageviews
+if (pagename == curr_pagename): # print last count
+    print(curr_pagename, '\t', dates, '\t', pageviews_per_date, '\t', count)
     
-for pagename_date, pageviews in view_count.items():
-    print(pagename_date, pageviews)
+#     # print old result, and start accumulating new result
+#     if (pagename_date != curr_pagename_date):
+#         if (curr_pagename_date != None): # print old result
+#             print(curr_pagename_date, count)
+#         curr_pagename_date = pagename_date
+#         count = pageviews
+#     else:
+#         count += pageviews
+# if (pagename_date == curr_pagename_date): # print last count
+#     print(curr_pagename_date, count)
+
