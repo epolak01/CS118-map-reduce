3bf1af6394c7a703717b8f31302346d4b567785f
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-12-07 16:27:47 -0500

    fixed regex warning

--- mapper1.py
+++ mapper1.py
@@ -9,15 +9,14 @@ for line in sys.stdin:
     except:
         continue
     
-    # filename = os.getenv('mapreduce_map_input_file')
-    
-    # name, date, hours = filename.split('-')
+    filename = os.getenv('mapreduce_map_input_file')
+    name, date, hours = filename.split('-')
     date = "20160601" # temporary, get date from filename 
     pagename_decoded = urllib.parse.unquote_plus(pagename)  ## for lines that have this part only
     
     if re.search(r'^en', projectcode): # TODO: is it just beginning with en or just en??????
         if not re.match(r'^(Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)', pagename_decoded):  ## ^ = begins with
-            if not re.search(r'^[[:upper:]]', pagename_decoded):
+            if not re.search(r'^[\[:upper:\]]', pagename_decoded):
                 if not re.search(r'(.jpg|.gif|.png|.ico|.txt)$', pagename_decoded, re.I):  ## re.I = case-insensitive
                     if not re.search(r'(404_error|Main_Page|Hypertext_Transfer_Protocol|Favicon.ico|Search)', pagename_decoded):
                         print(f"{pagename_decoded}'}'{date}\t{pageviews}")
