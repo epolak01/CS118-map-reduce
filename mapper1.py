dd8d7ceab75c385870606b9b014ac1daaa017924
Morgan Rockett <morgan.rockett@tufts.edu>
2022-12-09 18:19:43 -0500

    edit for 2nd if

--- mapper1.py
+++ mapper1.py
@@ -16,7 +16,7 @@ def main(argv):
         pagename_decoded = urllib.parse.unquote_plus(pagename)  ## for lines that have this part only
         
         if re.search(r'^en$', projectcode): # TODO: is it just beginning with en or just en??????
-            if not re.search(r'^(Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)', pagename_decoded):  ## ^ = begins with
+            if not re.search(r'^\"*Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk', pagename_decoded):  ## ^ = begins with
                 if not re.search(r'^\"*[\[:upper:\]]', pagename_decoded):
                     if not re.search(r'(.jpg|.gif|.png|.ico|.txt)\"*$', pagename_decoded, re.I):  ## re.I = case-insensitive
                         if not re.search(r'(404_error|Main_Page|Hypertext_Transfer_Protocol|Favicon.ico|Search)', pagename_decoded):
