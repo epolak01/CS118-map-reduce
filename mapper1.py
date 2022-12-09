47b5c48338441eb24be5cdc8386c8049da8f7309
Morgan Rockett <morgan.rockett@tufts.edu>
2022-12-09 18:21:14 -0500

    consistency update

--- mapper1.py
+++ mapper1.py
@@ -16,24 +16,13 @@ def main(argv):
         pagename_decoded = urllib.parse.unquote_plus(pagename)  ## for lines that have this part only
         
         if re.search(r'^en$', projectcode): # TODO: is it just beginning with en or just en??????
-            if not re.search(r'^\"*Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk', pagename_decoded):  ## ^ = begins with
+            if not re.search(r'^\"*(Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)', pagename_decoded):  ## ^ = begins with
                 if not re.search(r'^\"*[\[:upper:\]]', pagename_decoded):
                     if not re.search(r'(.jpg|.gif|.png|.ico|.txt)\"*$', pagename_decoded, re.I):  ## re.I = case-insensitive
                         if not re.search(r'(404_error|Main_Page|Hypertext_Transfer_Protocol|Favicon.ico|Search)', pagename_decoded):
                             print(f"{pagename_decoded}}}{date}\t{pageviews}")
 
-        """ we now take into account url decoding has quotations at the beginning and end; \"* means optional quotation mark
-        if re.search(r'^en', projectcode): # TODO: is it just beginning with en or just en??????
-            if not re.match(r'^(Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)', pagename_decoded):  ## ^ = begins with
-                if not re.search(r'^[\[:upper:\]]', pagename_decoded):
-                    if not re.search(r'(.jpg|.gif|.png|.ico|.txt)$', pagename_decoded, re.I):  ## re.I = case-insensitive
-                        if not re.search(r'(404_error|Main_Page|Hypertext_Transfer_Protocol|Favicon.ico|Search)', pagename_decoded):
-                            print(f"{pagename_decoded}}}{date}\t{pageviews}")
-        """
-
 
 if __name__ == "__main__":
     main(sys.argv)
-
-
 
