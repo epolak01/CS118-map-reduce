d237ddbf2761402e3af5e07dcbcc5d52a2ccf852
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-12-11 23:26:33 -0500

    added sorting for lists in reducer2

--- reducer2.py
+++ reducer2.py
@@ -5,8 +5,9 @@ import sys
 def main(argv):
     pagename = None
     curr_pagename = None
-    dates = []
-    pageviews_per_date = []
+    # dates = []
+    # pageviews_per_date = []
+    dates_and_views = []
     count = 0
     pop_trend = 0
 
@@ -21,17 +22,22 @@ def main(argv):
             continue
         # print old result, and start accumulating new result
         if (pagename != curr_pagename):
-            if (curr_pagename != None): # print old result            
+            if (curr_pagename != None): # print old result
+                # HAVE TO SORT DATE BUT ALSO PAGEVIEWS
+                dates_and_views.sort(key=lambda x: x[0])
+                dates, pageviews_per_date = list(zip(*dates_and_views))
                 print(f"{curr_pagename}\t[{','.join(map(str, dates))}]\t[{','.join(map(str, pageviews_per_date))}]\t{count}\t{pop_trend}")
             
             # reset variables from previous key
-            dates.clear()
-            pageviews_per_date.clear()
+            # dates.clear()
+            # pageviews_per_date.clear()
+            dates_and_views.clear()
             
             # set variables for next key
             curr_pagename = pagename
-            dates.append(date)
-            pageviews_per_date.append(pageviews)
+            dates_and_views.append((date, pageviews))
+            # dates.append(date)
+            # pageviews_per_date.append(pageviews)
             count = pageviews
             
             if (date % 100 > 2):
@@ -40,13 +46,16 @@ def main(argv):
                 pop_trend = -pageviews
         else:
             count += pageviews
-            dates.append(date)
-            pageviews_per_date.append(pageviews)
+            dates_and_views.append((date, pageviews))
+            # dates.append(date)
+            # pageviews_per_date.append(pageviews)
             if (date % 100 > 2):
                 pop_trend += pageviews
             else:
                 pop_trend -= pageviews
     if (pagename == curr_pagename): # print last count
+        dates_and_views.sort(key=lambda x: x[0])
+        dates, pageviews_per_date = list(zip(*dates_and_views))
         print(f"{curr_pagename}\t[{','.join(map(str, dates))}]\t[{','.join(map(str, pageviews_per_date))}]\t{count}\t{pop_trend}")
     
 if __name__ == "__main__":
